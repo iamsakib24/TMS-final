fa6f59bb6fa0db029f260caf2bcd6d6d8bcb0379
MD Sakib Al Hasan <iamsakib360@gmail.com>
2026-04-25 10:51:29 +0600

    Update search.php

--- search.php
+++ search.php
@@ -2,175 +2,127 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['trmsaid']==0)) {
-  header('location:logout.php');
-  } else{
 
 
+if(isset($_POST['submit']))
+{
+    $teacher_id = $_POST['teacher_id'];
+    $salary = $_POST['salary'];
+    $month = $_POST['month'];
+    $year = $_POST['year'];
 
-  ?>
-<!doctype html>
-<html class="no-js" lang="en">
+    // Duplicate check 
+    $checksql = "SELECT * FROM tblsalary 
+                 WHERE teacher_id=:tid AND month=:month AND year=:year";
+    $checkquery = $dbh->prepare($checksql);
+    $checkquery->bindParam(':tid',$teacher_id,PDO::PARAM_STR);
+    $checkquery->bindParam(':month',$month,PDO::PARAM_STR);
+    $checkquery->bindParam(':year',$year,PDO::PARAM_STR);
+    $checkquery->execute();
+
+    if($checkquery->rowCount() > 0){
+        echo "<script>alert('Salary already added for this month');</script>";
+    } else {
+
+        $sql = "INSERT INTO tblsalary(teacher_id, salary, month, year)
+                VALUES(:teacher_id, :salary, :month, :year)";
+        
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':teacher_id',$teacher_id,PDO::PARAM_STR);
+        $query->bindParam(':salary',$salary,PDO::PARAM_STR);
+        $query->bindParam(':month',$month,PDO::PARAM_STR);
+        $query->bindParam(':year',$year,PDO::PARAM_STR);
+
+        $query->execute();
+
+        echo "<script>alert('Salary Added Successfully');</script>";
+    }
+}
+?><!doctype html>
+<html lang="en">
 <head>
-   
-    <title>TRMS Search</title>
-    
+    <title>Teacher Salary Management</title>
 
-    <link rel="apple-touch-icon" href="apple-icon.png">
-    <link rel="shortcut icon" href="favicon.ico">
-
-
-    <link rel="stylesheet" href="vendors/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="vendors/themify-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="vendors/selectFX/css/cs-skin-elastic.css">
-
-    <link rel="stylesheet" href="assets/css/style.css">
-
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-
-
-
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        td {
+            border: 2px solid orange;
+        }
+        th {
+            color:#000;
+            border: 2px solid orange;
+            font-weight:bold;
+            font-size:18px;
+        }
+    </style>
 </head>
 
 <body>
-    <!-- Left Panel -->
 
-    <?php include_once('includes/sidebar.php');?>
+<?php include_once('includes/header.php');?>
 
-    <div id="right-panel" class="right-panel">
+<div class="container mt-5">
+    <h2 class="text-center">Teacher Salary Management</h2>
 
-        <!-- Header-->
-        <?php include_once('includes/header.php');?>
-        <div class="breadcrumbs">
-            <div class="col-sm-4">
-                <div class="page-header float-left">
-                    <div class="page-title">
-                        <h1>Search Teacher</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-8">
-                <div class="page-header float-right">
-                    <div class="page-title">
-                        <ol class="breadcrumb text-right">
-                            <li><a href="dashboard.php">Dashboard</a></li>
-                            <li><a href="search.php">Search Teacher</a></li>
-                            <li class="active">Teacher</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <table class="table table-responsive">
+        <thead>
+            <tr>
+                <th>S.No</th>
+                <th>Name</th>
+                <th>Subject</th>
+                <th>Salary</th>
+                <th>Month</th>
+                <th>Year</th>
+                <th>Action</th>
+            </tr>
+        </thead>
 
-        <div class="content mt-3">
-            <div class="animated fadeIn">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <strong class="card-title">Search Teacher</strong>
-                            </div>
+        <?php
+        $sql = "SELECT * FROM tblteacher";
+        $query = $dbh->prepare($sql);
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_OBJ);
 
-<form name="search" method="post" style="padding-top: 20px" >
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card-box">
-                                   
-                                   
-                                       <div class="form-group row">
-                                                        <label class="col-4 col-form-label" for="example-email" style="padding-left: 50px"><strong>Search by Name or Subject</strong></label>
-                                                        <div class="col-6">
-                                                            <input id="searchdata" type="text" name="searchdata" required="true" class="form-control">
-                                                        </div>
-                                                    </div>
-                                                    <p style="text-align: center;"><button type="submit" class="btn btn-primary btn-sm" name="search" id="submit">
-                                                            <i class="fa fa-dot-circle-o"></i> Search
-                                                        </button></p> 
+        $cnt = 1;
+        foreach($results as $row)
+        {
+        ?>
+        <form method="post">
+        <tr>
+            <td><?php echo $cnt;?></td>
+            <td><?php echo $row->Name;?></td>
+            <td><?php echo $row->TeacherSub;?></td>
 
-                                                    </div> 
-                                    
-       </form>
+            <td>
+                <input type="text" name="salary" class="form-control" required>
+            </td>
 
+            <td>
+                <input type="text" name="month" class="form-control" placeholder="April" required>
+            </td>
 
-<?php
-if(isset($_POST['search']))
-{ 
+            <td>
+                <input type="text" name="year" class="form-control" placeholder="2026" required>
+            </td>
 
-$sdata=$_POST['searchdata'];
-  ?>
-  <h4 align="center">Result against "<?php echo $sdata;?>" keyword </h4> 
+            <td>
+                <input type="hidden" name="teacher_id" value="<?php echo $row->ID;?>">
+                <button type="submit" name="submit" class="btn btn-success">Save</button>
+            </td>
+        </tr>
+        </form>
 
+        <?php 
+        $cnt++;
+        }
+        ?>
+    </table>
+</div>
 
-                            <div class="card-body">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <tr>
-                  <th>S.NO</th>
-            
-                  <th>Teacher Name</th>
-                  <th>Subject</th>
-                    <th>Registration Date</th>       
-                   <th>Action</th>
-                </tr>
-                                        </tr>
-                                        </thead>
-                                    <?php
-
-$sql="SELECT * from tblteacher where Name like '%$sdata%' || TeacherSub like  '%$sdata%'";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $row)
-{               ?>   
-              
-                <tr>
-                  <td><?php echo htmlentities($cnt);?></td>
-            <td><?php  echo htmlentities($row->Name);?></td>
-                  <td><?php  echo htmlentities($row->TeacherSub);?></td>
-                  <td><?php  echo htmlentities($row->RegDate);?></td>
-                  <td><a href="edit-teacher-detail.php?editid=<?php echo htmlentities ($row->ID);?>">Edit Details</a></td>
-                </tr>
-                 <?php 
-$cnt=$cnt+1;
-} } else { ?>
-  <tr>
-    <td colspan="8"> No record found against this search</td>
-
-  </tr>
-   
-<?php } }?>
-
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                </div>
-            </div><!-- .animated -->
-        </div><!-- .content -->
-
-
-    </div><!-- /#right-panel -->
-
-    <!-- Right Panel -->
-
-
-    <script src="vendors/jquery/dist/jquery.min.js"></script>
-    <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
-    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="assets/js/main.js"></script>
 
 
 </body>
-
 </html>
-<?php }  ?>
