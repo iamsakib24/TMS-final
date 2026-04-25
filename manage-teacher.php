06a541a0af307584ea98b4a165022bc74f3dba3c
MD Sakib Al Hasan <iamsakib360@gmail.com>
2026-04-25 10:49:17 +0600

    Update manage-teacher.php

--- manage-teacher.php
+++ manage-teacher.php
@@ -2,134 +2,116 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
+
 if (strlen($_SESSION['trmsaid']==0)) {
   header('location:logout.php');
-  } else{
-
-
-
-  ?>
+} else {
+?>
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
-    
     <title>TRMS||||Manage Teacher</title>
-    
-    <link rel="apple-touch-icon" href="apple-icon.png">
-  
-
 
     <link rel="stylesheet" href="vendors/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="vendors/themify-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="vendors/selectFX/css/cs-skin-elastic.css">
-
     <link rel="stylesheet" href="assets/css/style.css">
-
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-
-
-
 </head>
 
 <body>
-    <!-- Left Panel -->
 
-    <?php include_once('includes/sidebar.php');?>
+<?php include_once('includes/sidebar.php');?>
 
-    <div id="right-panel" class="right-panel">
+<div id="right-panel" class="right-panel">
 
-        <!-- Header-->
-        <?php include_once('includes/header.php');?>
-        <div class="breadcrumbs">
-            <div class="col-sm-4">
-                <div class="page-header float-left">
-                    <div class="page-title">
-                        <h1>Manage Teachers</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-8">
-                <div class="page-header float-right">
-                    <div class="page-title">
-                        <ol class="breadcrumb text-right">
-                            <li><a href="dashboard.php">Dashboard</a></li>
-                            <li><a href="manage-teacher.php">Manage Teachers</a></li>
-                            <li class="active">Manage Teachers</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
+<?php include_once('includes/header.php');?>
 
-        <div class="content mt-3">
-            <div class="animated fadeIn">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <strong class="card-title">Manage Teachers</strong>
-                            </div>
-                            <div class="card-body">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <tr>
-                  <th>S.NO</th>
-            
-                  <th>Teacher Name</th>
-                  <th>Subject</th>
-                    <th>Registration Date</th>       
-                   <th>Action</th>
-                </tr>
-                                        </tr>
-                                        </thead>
-                                    <?php
-$sql="SELECT * from tblteacher";
-$query = $dbh -> prepare($sql);
+<div class="breadcrumbs">
+    <div class="col-sm-4">
+        <h1>Manage Teachers</h1>
+    </div>
+</div>
+
+<div class="content mt-3">
+<div class="animated fadeIn">
+<div class="row">
+<div class="col-lg-12">
+<div class="card">
+<div class="card-header">
+    <strong class="card-title">Manage Teachers (With Salary)</strong>
+</div>
+
+<div class="card-body">
+<table class="table table-bordered">
+<thead>
+<tr>
+  <th>S.NO</th>
+  <th>Teacher Name</th>
+  <th>Subject</th>
+  <th>Salary</th>
+  <th>Month</th>
+  <th>Year</th>
+  <th>Registration Date</th>       
+  <th>Action</th>
+</tr>
+</thead>
+
+<?php
+// ✅ UPDATED QUERY (JOIN)
+$sql="SELECT tblteacher.*, tblsalary.salary, tblsalary.month, tblsalary.year 
+      FROM tblteacher 
+      LEFT JOIN tblsalary 
+      ON tblteacher.ID = tblsalary.teacher_id";
+
+$query = $dbh->prepare($sql);
 $query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
+$results = $query->fetchAll(PDO::FETCH_OBJ);
 
 $cnt=1;
+
 if($query->rowCount() > 0)
 {
 foreach($results as $row)
-{               ?>   
-              
-                <tr>
-                  <td><?php echo htmlentities($cnt);?></td>
-            <td><?php  echo htmlentities($row->Name);?></td>
-                  <td><?php  echo htmlentities($row->TeacherSub);?></td>
-                  <td><?php  echo htmlentities($row->RegDate);?></td>
-                  <td><a href="edit-teacher-detail.php?editid=<?php echo htmlentities ($row->ID);?>">Edit Details</a></td>
-                </tr>
-               <?php $cnt=$cnt+1;}} ?>   
+{   
+?>
+<tr>
+  <td><?php echo htmlentities($cnt);?></td>
+  <td><?php echo htmlentities($row->Name);?></td>
+  <td><?php echo htmlentities($row->TeacherSub);?></td>
 
-                                </table>
-                            </div>
-                        </div>
-                    </div>
+  <!-- ✅ Salary Show -->
+  <td><?php echo $row->salary ? $row->salary : 'Not Added'; ?></td>
+  <td><?php echo $row->month ? $row->month : '-'; ?></td>
+  <td><?php echo $row->year ? $row->year : '-'; ?></td>
 
+  <td><?php echo htmlentities($row->RegDate);?></td>
 
+  <td>
+    <a href="edit-teacher-detail.php?editid=<?php echo htmlentities($row->ID);?>">
+        Edit
+    </a>
+  </td>
+</tr>
 
-                </div>
-            </div><!-- .animated -->
-        </div><!-- .content -->
+<?php 
+$cnt++;
+}} 
+?>
 
+</table>
+</div>
 
-    </div><!-- /#right-panel -->
+</div>
+</div>
+</div>
+</div>
+</div>
 
-    <!-- Right Panel -->
+</div>
 
-
-    <script src="vendors/jquery/dist/jquery.min.js"></script>
-    <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
-    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="assets/js/main.js"></script>
-
+<script src="vendors/jquery/dist/jquery.min.js"></script>
+<script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 
 </body>
-
 </html>
-<?php }  ?>
+<?php } ?>
